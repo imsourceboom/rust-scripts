be7965b24d681f8bb50879d74918af335eb2b3c7
imsourceboom <ecmascript.developer@gmail.com>
2021-12-15 15:15:33 +0900

    updated key rename script

--- scripts/setup/keysRename.sh
+++ scripts/setup/keysRename.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+cd $HOME
+
+PACKAGE=$(dpkg -l | grep rename)
+if [ -z ${PACKAGE} ]; then
+	sudo apt install rename -y
+fi
+
 cd $HOME/keys
 
 OLD_HOSTNAME=$(ls *.depool.addr | cut -f 1 -d '.')
 rename "s/${OLD_HOSTNAME}\.//" * && \
 	mv addr validator.addr && \
-	rm ${OLD_HOSTNAME}.depool.addr
-
-ls
+	rm ${OLD_HOSTNAME}.depool.addr && \
+	ls
