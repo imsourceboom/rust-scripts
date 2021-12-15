51991b151a23e2b95898f21ba2649c33514772b6
imsourceboom <ecmascript.developer@gmail.com>
2021-12-15 15:19:30 +0900

    update key copy script

--- scripts/setup/keysCopy.sh
+++ scripts/setup/keysCopy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 KEYS_PATH="$HOME/keys"
-#DEPOOL_KEY_PATH="$HOME/latest-depool-key"
 
 docker cp $KEYS_PATH/validator.addr rnode:/ton-node/configs/my_validator.addr
-docker exec -it rnode mkdir /ton-node/configs/keys
+#docker exec -it rnode mkdir /ton-node/configs/keys
 docker cp $KEYS_PATH/msig.keys.json rnode:/ton-node/configs/keys
 docker cp $KEYS_PATH/depool.addr rnode:/ton-node/configs
