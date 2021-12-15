afe20bb665236ab7c95bf0b93cc49e1e6d474b97
imsourceboom <ecmascript.developer@gmail.com>
2021-12-15 14:55:58 +0900

    modified key rename script

--- scripts/setup/keysRename.sh
+++ scripts/setup/keysRename.sh
@@ -2,7 +2,7 @@
 
 cd $HOME/keys
 
-OLD_HOSTNAME=$(ls *.depool.addr | cur -f 1 -d '.')
+OLD_HOSTNAME=$(ls *.depool.addr | cut -f 1 -d '.')
 rename "s/${OLD_HOSTNAME}\.//" * && \
 	mv addr validator.addr && \
 	rm ${OLD_HOSTNAME}.depool.addr
