b8bca608a9abd5ddd9bade548f298ff366066615
imsourceboom <ecmascript.developer@gmail.com>
2021-12-19 21:54:43 +0900

    update confirm script

--- scripts/rust-confirm-transaction.sh
+++ scripts/rust-confirm-transaction.sh
@@ -17,7 +17,7 @@ do
    DESTINATION_ADDR=$(echo $GET_TRANSACTIONS | awk -F"Result: " '{print $2}' | jq ".transactions[$i].dest" | tr -d \")
    TRANSACTION_ID=$(echo $GET_TRANSACTIONS | awk -F"Result: " '{print $2}' | jq ".transactions[$i].id")
    if [ $DESTINATION_ADDR == $DEPOOL_ADDR ] && [ $DESTINATION_ADDR == $HELPER_ADDR ]; then
-      $TONOS_CLI -c $TONOS_CLI_CONFIG  call $VALIDATOR_ADDR confirmTransaction "{\"transactionId\":\"$TRANSACTION_ID\"}" --abi $SAFE_ABI --sign $CUSTODIAN_KEY
+      $TONOS_CLI -c $TONOS_CLI_CONFIG  call $VALIDATOR_ADDR confirmTransaction "{\"transactionId\":$TRANSACTION_ID}" --abi $SAFE_ABI --sign "$CUSTODIAN_KEY"
    else
       echo "WARNING: unknown destination $DESTINATION_ADDR for wallet $VALIDATOR_ADDR"
    fi
