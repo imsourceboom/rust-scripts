f01f0cef1aae8fcf5527ea44c5e26ed2ef0ee842
imsourceboom <ecmascript.developer@gmail.com>
2021-12-20 12:13:33 +0900

    update confirm transaction script

--- scripts/rust-confirm-transaction.sh
+++ scripts/rust-confirm-transaction.sh
@@ -2,8 +2,6 @@
 
 . $HOME/rust-scripts/scripts/rust-env.sh
 
-CUSTODIAN_KEY=""
-
 GET_TRANSACTIONS="$($TONOS_CLI -c $TONOS_CLI_CONFIG run $VALIDATOR_ADDR getTransactions {} --abi $SAFE_ABI)" 
 TRANSACTIONS_COUNT=$(echo $GET_TRANSACTIONS | awk -F'Result: ' '{print $2}' | jq '.transactions|length')
 
@@ -12,13 +10,18 @@ if [[ $TRANSACTIONS_COUNT == 0 ]]; then
    exit 0
 fi
 
+$TONOS_CLI -c $TONOS_CLI_CONFIG getkeypair $KEYS_DIR/confirm.keys.json ""
+CONFIRM_KEYS_JSON="$KEYS_DIR/confirm.keys.json"
+
 for (( i=0; i<$TRANSACTIONS_COUNT; i++ ))
 do  
    DESTINATION_ADDR=$(echo $GET_TRANSACTIONS | awk -F"Result: " '{print $2}' | jq ".transactions[$i].dest" | tr -d \")
    TRANSACTION_ID=$(echo $GET_TRANSACTIONS | awk -F"Result: " '{print $2}' | jq ".transactions[$i].id")
    if [ $DESTINATION_ADDR == $DEPOOL_ADDR ]; then
-      $TONOS_CLI -c $TONOS_CLI_CONFIG  call $VALIDATOR_ADDR confirmTransaction "{\"transactionId\":$TRANSACTION_ID}" --abi $SAFE_ABI --sign "$CUSTODIAN_KEY"
+      $TONOS_CLI -c $TONOS_CLI_CONFIG  call $VALIDATOR_ADDR confirmTransaction "{\"transactionId\":$TRANSACTION_ID}" --abi $SAFE_ABI --sign $CONFIRM_KEYS_JSON
    else
       echo "WARNING: unknown destination $DESTINATION_ADDR for wallet $VALIDATOR_ADDR"
    fi
 done
+
+rm $CONFIRM_KEYS_JSON
