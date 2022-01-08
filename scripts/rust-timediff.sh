c897150af6aaf88ecaa7018b9f197af7ad43b330
imsourceboom <ecmascript.developer@gmail.com>
2022-01-08 11:17:53 +0900

    update timediff scripts

--- scripts/rust-timediff.sh
+++ scripts/rust-timediff.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-TIMEDIFF=$(docker exec -t rnode /ton-node/tools/console -C /ton-node/configs/console.json -c getstats | grep 'timediff' | awk '{print $2}' | tr -d ,)
+TIMEDIFF=$(docker exec -t rnode /ton-node/tools/console -C /ton-node/configs/console.json -jc getstats | jq -r '.timediff')
 echo $TIMEDIFF
