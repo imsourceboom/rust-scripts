36e4913441c6b722d12fb094d7f8fd1c30106e22
imsourceboom <ecmascript.developer@gmail.com>
2022-01-10 23:26:40 +0900

    updated clean log script

--- scripts/rust-clean-log.sh
+++ scripts/rust-clean-log.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-cd $HOME/rustnet.ton.dev/docker-compose/ton-node/logs
+LOGS_DIR="$HOME/rustnet.ton.dev/docker-compose/ton-node/logs"
 
-echo 0 | sudo tee *
+if [ -f $LOGS_DIR/output.log ]; then
+	echo 0 | sudo tee $LOGS_DIR/output.log
+fi
+
+if [ -f $LOGS_DIR/output_1.log ]; then
+	echo 0 | sudo tee $LOGS_DIR/output_1.log
+fi
+
+if [ -f $LOGS_DIR/send_depool_tick_tock.log ]; then
+	echo 0 | sudo tee $LOGS_DIR/send_depool_tick_tock.log
+fi
+
+if [ -f $LOGS_DIR/stderr.log ]; then
+	echo 0 | sudo tee $LOGS_DIR/stderr.log
+fi
+
+if [ -f $LOGS_DIR/stdout.log ]; then
+	echo 0 | sudo tee $LOGS_DIR/stdout.log
+fi
+
+if [ -f $LOGS_DIR/validator.log ]; then
+	echo 0 | sudo tee $LOGS_DIR/validator.log
+fi
+
