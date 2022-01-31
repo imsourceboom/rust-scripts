4a5e2ff4e36c9f7b23417b92d17418703eec8c61
imsourceboom <ecmascript.developer@gmail.com>
2022-01-31 20:36:20 +0900

    update db check monitoring

--- monitoring/checkList/dbCheck.js
+++ monitoring/checkList/dbCheck.js
@@ -7,7 +7,7 @@ cron.scheduleJob(`${second} */10 * * * *`, () => {
   shell.cd('~/rust-scripts/scripts');
   const dbAvail = Number(shell.exec('./db-check.sh', { silent: true }).stdout);
 
-  if (dbAvail <= 130) {
+  if (dbAvail <= 132) {
     bot.sendMessage(electionChatId, `${serverNo}💾 DB Avail: ${dbAvail}G\nStart Database clean up!\nrestart PM2 after about 4 hours`);
     shell.exec('./rust-clean-db.sh', { silent: true });
   }
