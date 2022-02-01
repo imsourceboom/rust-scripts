cdf69633016653243153a1290043722bfd581613
imsourceboom <ecmascript.developer@gmail.com>
2022-02-02 01:15:29 +0900

    create rust node upgrade scripts

---
+++ scripts/rust-node-upgrade.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SERVER_NO=$(cat $HOME/serverno)
+GROUP_NO=0
+
+PREV=0
+
+cd $HOME/main.ton.dev/scripts
+tmux new -ds upgarde
+
+for (( i = 3; i <= 198; i+=3 ))
+do
+	if [ $SERVER_NO -gt $PREV -a $SERVER_NO -le $i ]; then
+		GROUP_NO=$(( $i / 3 - 1 ))
+		tmux send -t upgrade.0 "sleep $((1200 * $GROUP_NO)) && ./upgrade.sh 2>&1 | tee ./upgrade.log && sleep 100 && getstats | grep -E 'console|COMMIT_ID|node_version'" ENTER
+	fi
+
+	PREV=$i
+done
