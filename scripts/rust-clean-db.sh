3c1aece621f222fd4123e6463af5c29eaec468f1
imsourceboom <ecmascript.developer@gmail.com>
2022-02-03 18:30:44 +0900

    update clean db scripts

--- scripts/rust-clean-db.sh
+++ scripts/rust-clean-db.sh
@@ -4,4 +4,4 @@ DOCKER_DIR="$HOME/main.ton.dev/docker-compose/ton-node"
 
 cd $DOCKER_DIR
 tmux new -ds clear
-tmux send -t clear.0 "df -h / && pm2 stop 0 && docker-compose down -v && docker-compose up -d && df -h / && sleep 18000 && pm2 restart 0" ENTER
+tmux send -t clear.0 "echo 'scripts start' && date '+%F %a %T' && df -h / && pm2 stop 0 && docker-compose down -v && docker-compose up -d && df -h / && sleep 18000 && pm2 restart 0" ENTER
