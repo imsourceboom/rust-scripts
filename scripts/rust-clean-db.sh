4c12b684b3af22f281f1cbbc6b19429cb0003d90
imsourceboom <ecmascript.developer@gmail.com>
2022-02-03 23:51:16 +0900

    update clean db scripts

--- scripts/rust-clean-db.sh
+++ scripts/rust-clean-db.sh
@@ -3,9 +3,10 @@
 DOCKER_DIR="$HOME/main.ton.dev/docker-compose/ton-node"
 TOOLS_DIR="$HOME/rust-scripts/tools"
 
+sed -i "s|DEPOOL_ENABLE=.*|DEPOOL_ENABLE=yes|g" $DOCKER_DIR/.env
+cat $DOCKER_DIR/.env | tail -n 1
+
 cd $DOCKER_DIR
 tmux new -ds clean
-tmux send -t clean.0 "docker cp rnode:/ton-node/tools/tonos-cli.conf.json $TOOLS_DIR" ENTER
-tmux send -t clean.0 "echo 'scripts start' && date '+%F %a %T' && df -h / && pm2 stop 0 && docker-compose down -v && docker-compose up -d && df -h / && sleep 5" ENTER
-tmux send -t clean.0 "docker cp $TOOLS_DIR/tonos-cli.conf.json rnode:/ton-node/tools" ENTER
+tmux send -t clean.0 "echo 'scripts start' && date '+%F %a %T' && df -h / && pm2 stop 0 && docker-compose down -v && docker-compose up -d && df -h /" ENTER
 tmux send -t clean.0 "sleep 14400 && pm2 restart 0" ENTER
