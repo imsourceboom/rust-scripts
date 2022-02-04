d9cd4d70a9eab2df880ba471c3501ed2b4eae066
imsourceboom <ecmascript.developer@gmail.com>
2022-02-04 15:43:38 +0900

    update getDePoolInfo scripts

--- scripts/getDePoolInfo.sh
+++ scripts/getDePoolInfo.sh
@@ -4,10 +4,12 @@
 
 GET_DEPOOL_INFO=$($TONOS run $DEPOOL_ADDR getDePoolInfo {} --abi $DEPOOL_ABI)
 
-if [ $1 == "minStake" ]; then
-	MIN_STAKE=$(echo $GET_DEPOOL_INFO | awk -F 'Result: ' '{print $2}' | jq -r ".minStake" | xargs hex2gram.sh)
-	echo $MIN_STAKE
-	exit
+if [ $# -gt 0 ]; then
+	if [ $1 == "minStake" ]; then
+		MIN_STAKE=$(echo $GET_DEPOOL_INFO | awk -F 'Result: ' '{print $2}' | jq -r ".minStake" | xargs hex2gram.sh)
+		echo $MIN_STAKE
+		exit
+	fi
 fi
 
 echo "$GET_DEPOOL_INFO"
