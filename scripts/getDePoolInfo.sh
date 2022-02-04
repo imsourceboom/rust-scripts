0fe5d23f0e93c318bb69228aeaadcd7b860517b2
imsourceboom <ecmascript.developer@gmail.com>
2022-02-04 15:39:38 +0900

    update getDePoolInfo scripts

--- scripts/getDePoolInfo.sh
+++ scripts/getDePoolInfo.sh
@@ -2,6 +2,12 @@
 
 . $HOME/rust-scripts/scripts/rust-env.sh
 
-$TONOS run $DEPOOL_ADDR \
-        getDePoolInfo {} \
-        --abi $DEPOOL_ABI
+GET_DEPOOL_INFO=$($TONOS run $DEPOOL_ADDR getDePoolInfo {} --abi $DEPOOL_ABI)
+
+if [ $1 == "minStake" ]; then
+	MIN_STAKE=$(echo $GET_DEPOOL_INFO | awk -F 'Result: ' '{print $2}' | jq ".minStake" | xargx hex2gram.sh)
+	echo $MIN_STAKE
+	exit
+fi
+
+echo $GET_DEPOOL_INFO
--- scripts/ordinaryStake.sh
+++ scripts/ordinaryStake.sh
@@ -2,9 +2,15 @@
 
 . $HOME/rust-scripts/scripts/rust-env.sh
 
+if [ $# -eq 0 ]; then
+	echo "Parameter is Required !"
+	echo "ordinaryStake.sh [stake - gram]"
+	exit
+fi
+
 $TONOS_CLI -c $TONOS_CLI_CONFIG depool \
        --addr $DEPOOL_ADDR \
        stake ordinary \
        --wallet $VALIDATOR_ADDR \
-       --value 1000000 \
+       --value $1 \
        --sign $MSIG_KEYS_JSON_PATH
