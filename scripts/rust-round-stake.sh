8565491ea9f11bf279e0dc4a9bc1e6460de18727
imsourceboom <ecmascript.developer@gmail.com>
2022-03-04 11:39:09 +0900

    update round stake scripts

--- scripts/rust-round-stake.sh
+++ scripts/rust-round-stake.sh
@@ -2,10 +2,15 @@
 
 . $HOME/rust-scripts/scripts/rust-env.sh
 
-GET_ROUNDS_STAKE=$(./getRounds.sh |  grep '"stake":')
-ODD_ROUND_STAKE=$(echo $GET_ROUNDS_STAKE | awk '{print $2}' | tr -d '",' | xargs hex2gram.sh)
-EVEN_ROUND_STAKE=$(echo $GET_ROUNDS_STAKE | awk '{print $4}' | tr -d '",' | xargs hex2gram.sh)
+GET_ROUNDS=$(./getRounds.sh)
+STAKE=$(echo "$GET_ROUNDS" |  grep '"stake":')
+VALIDATOR_STAKE=$(echo "$GET_ROUNDS" |  grep '"validatorStake":')
+TOTAL_ODD_ROUND_STAKE=$(echo $STAKE | awk '{print $2}' | tr -d '",' | xargs hex2gram.sh)
+TOTAL_EVEN_ROUND_STAKE=$(echo $STAKE | awk '{print $4}' | tr -d '",' | xargs hex2gram.sh)
+VALIDATOR_ODD_ROUND_STAKE=$(echo $VALIDATOR_STAKE | awk '{print $2}' | tr -d '",' | xargs hex2gram.sh)
+VALIDATOR_EVEN_ROUND_STAKE=$(echo $VALIDATOR_STAKE | awk '{print $4}' | tr -d '",' | xargs hex2gram.sh)
 
-echo "Odd Round: $ODD_ROUND_STAKE"
-echo "Even Round: $EVEN_ROUND_STAKE"
-
+echo "Odd Round: $TOTAL_ODD_ROUND_STAKE"
+echo "Even Round: $TOTAL_EVEN_ROUND_STAKE"
+echo "Validator Odd Round: $VALIDATOR_ODD_ROUND_STAKE"
+echo "Validator Even Round: $VALIDATOR_EVEN_ROUND_STAKE"
