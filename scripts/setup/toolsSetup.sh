725705c725d5432ab4fe08e3f911aef9729a363f
imsourceboom <ecmascript.developer@gmail.com>
2022-09-21 14:29:45 +0900

    update url config

--- scripts/setup/toolsSetup.sh
+++ scripts/setup/toolsSetup.sh
@@ -18,7 +18,7 @@ docker cp rnode:/ton-node/configs/Elector.abi.json $RUST_SCRIPTS_DIR/tools
 # Check tonos-cli config json
 cd $TOOLS_DIR
 if [ ! -f "tonos-cli.conf.json" ]; then
-	./tonos-cli config endpoint add main.ton.dev "https://main2.ton.dev,https://main3.ton.dev,https://main4.ton.dev"
+	./tonos-cli config endpoint add main.ton.dev "https://eri01.main.everos.dev,https://gra01.main.everos.dev,https://gra02.main.everos.dev,https://lim01.main.everos.dev,https://rbx01.main.everos.dev"
 	./tonos-cli config --url main.ton.dev
 fi
 
