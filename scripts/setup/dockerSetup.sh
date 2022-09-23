7d7e4d2dfa6b251bc2d3f2991f92303ef208c216
imsourceboom <ecmascript.developer@gmail.com>
2022-09-23 14:00:26 +0900

    update docker setup scripts

--- scripts/setup/dockerSetup.sh
+++ scripts/setup/dockerSetup.sh
@@ -3,11 +3,11 @@
 cd $HOME
 
 sudo apt-get update -y
-sudo apt-get install curl jq -y
+sudo apt-get install curl jq wget bc tmux -y
 curl -fsSL https://get.docker.com/ | sudo sh
 sudo usermod -aG docker $USER
 
 echo '-----------------------'
-echo 'Please!! Reconnect to the server And install rustnet.ton.dev'
-echo 'git clone https://github.com/tonlabs/rustnet.ton.dev.git'
+echo 'Please!! Reconnect to the server And install main.ton.dev'
+echo 'git clone https://github.com/tonlabs/main.ton.dev.git'
 echo '-----------------------'
